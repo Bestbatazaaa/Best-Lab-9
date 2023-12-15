b472275e792b181caf54f8e95079ca397c23b468
Bestbatazaaa <Bestbatazaaa@icloud.com>
2023-12-15 14:44:52 +0700

    lab9_2

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,7 +1,21 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 //Write the function printO() here
+string printO(int line,int length){
+	if(line <= 0 or length <= 0 ){
+		cout << "Invalid input";
+	}else{
+		for(int i = line; i != 0; i--){
+			for(int i = length; i != 0; i--){
+				cout << "O";
+			}
+		cout << endl;
+	}
+	}
+	return "Invalid input";
+}
 
 int main(){
 	
